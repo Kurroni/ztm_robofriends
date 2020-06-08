aa91ae9fa232bf849fba4d20c076938c23d837d5
MichalR <mrudny@gmail.com>
2020-06-08 13:49:14 +0200

    make Card CarList components, import robots.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CardList from './CardList';
+import 'tachyons';
+import { robots } from './robots';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <CardList robots={robots}/>,
   document.getElementById('root')
 );
 
