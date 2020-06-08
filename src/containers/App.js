92f897b09aa50fe0fb14a47c90e47a0f3a23aa5a
MichalR <mrudny@gmail.com>
2020-06-08 18:19:22 +0200

    finish

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import { robots } from './robots';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends React.Component {
@@ -9,7 +9,7 @@ class App extends React.Component {
         super()
         this.state = {
             robots: [],
-            searchField: ''
+            searchfield: ''
         }
     }
 
@@ -20,25 +20,26 @@ class App extends React.Component {
     }
 
     onSearchChange = (event) => {
-        this.setState({ searchField: event.target.value})
+        this.setState({ searchfield: event.target.value})
     }
 
     render(){
-
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
-        if (this.state.robots.length === 0) {
-            return <h1>Loading...</h1>
-        } else {
-            return (
+        return !robots.length ?
+        <h1>Loading...</h1> :
+        (
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
             </div>
             )
         }
     }
-}
+
 export default App
