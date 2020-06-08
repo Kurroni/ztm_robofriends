b8bb26d8590f478cb4ee87de04890c1daeb317ce
MichalR <mrudny@gmail.com>
2020-06-08 15:21:23 +0200

    make functioning search box

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import 'tachyons';
-import { robots } from './robots';
-
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <CardList robots={robots}/>,
+  <App/>,
   document.getElementById('root')
 );
 
